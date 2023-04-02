9a04d865597540af69ad725128585ba1985c0a2e
swapi1802 <90041961+swapi1802@users.noreply.github.com>
2023-04-02 16:04:21 +0530

    2 files modified

--- index.js
+++ index.js
@@ -20,19 +20,38 @@ function logCar(){
   return this.color + " " + this.brand; 
 }
 
-let input = '{"color" : "red", "brand": "Mercedes"}\n {"color" : "white", "brand": "Toyota"}\n{"color" : "gray", "brand": "Audi"}';
-
-const data = input
-              .split('\n')
-              .filter((t) => !!t)
-              .map((i) => JSON.parse(i));
+const txtJson = document.getElementById('id_txtjson');
 const carlogId = document.getElementById('carlog_result');
 
-console.log('---------------- Task 2 Result --------------');
-for(let i = 0; i < data.length; i++){
-  let tempFun = logCar.bind(data[i]);
-  carlogId.append(tempFun() + ', '); // shown data on page too 
-}
+txtJson.value = '{"color" : "red", "brand": "Mercedes"}\n{"color" : "white", "brand": "Toyota"}\n{"color" : "gray", "brand": "Audi"}';
+
+const btnResult = document.getElementById('btn_add');
+btnResult.addEventListener("click", btnAddClick)
+document.getElementById('btn_clear').addEventListener("click", btnClearClick);
+
+function btnAddClick(evt){
+  carlogId.innerHTML = '';
+  if(txtJson.value != null && txtJson.value != ''){
+    const data = txtJson.value
+                .split('\n')
+                .filter((t) => !!t)
+                .map((i) => JSON.parse(i));
+    console.log('---------------- Task 2 Result --------------');
+    for(let i = 0; i < data.length; i++){
+      let tempFun = logCar.bind(data[i]);
+      carlogId.append(tempFun() + ', '); // shown data on page too 
+    }
+  }
+};
+
+function btnClearClick(evt){
+  txtJson.value = '';
+  carlogId.innerHTML = '';
+};
+
+
+btnResult.dispatchEvent(new Event('click'));
+
 
 // --------- Task 3 ------------
 
@@ -46,79 +65,119 @@ const Converter = {
   }
   };
 
-  const t3input = '1,2,4,2,4,5,5';
-  const radId = document.getElementById('rad_result');
-  let result = '';
-  t3input
-  .split(",")
-  .filter((t) => !!t)
-  .map((i) => parseInt(i, 10))
-  .forEach((ang) => {
-    let rad = Converter.deg2rad (ang);
-    const degPlus10= Converter.rad2deg(rad + 0.174533);
-    result += `${rad} - ${degPlus10}\n`;
-  });
+const txtTask3Values = document.getElementById('id_txttask3values');
+const radId = document.getElementById('rad_result');
 
-  radId.innerText = result;  
-  console.log('---------------- Task 3 Result --------------');
-  console.log(result);
+txtTask3Values.value = '1,2,4,2,4,5,5';
 
+const btnTask3Result = document.getElementById('btn_task3_result');
+btnTask3Result.addEventListener("click", btnTask3ReultClick)
+document.getElementById('btn_task3_clear').addEventListener("click", btnTask3ClearClick);
+
+function btnTask3ReultClick(evt){
+  radId.innerHTML = '';
+  if(txtTask3Values.value != null && txtTask3Values.value != ''){
+    let result = '';
+    txtTask3Values.value
+    .split(",")
+    .filter((t) => !!t)
+    .map((i) => parseInt(i, 10))
+    .forEach((ang) => {
+      let rad = Converter.deg2rad (ang);
+      const degPlus10= Converter.rad2deg(rad + 0.174533);
+      result += `${rad} - ${degPlus10}\n`;
+    });
+  
+    radId.innerText = result; 
+    console.log('---------------- Task 3 Result --------------');
+    console.log(result);     
+  }
+};
+
+function btnTask3ClearClick(evt){
+  txtTask3Values.value = '';
+  radId.innerHTML = '';
+};
+
+btnTask3Result.dispatchEvent(new Event('click'));
 
   // ------------ Task 4 ---------
 
-  
-let mapInput = '{"value" : "10", "targetDate": "2020-01-01"}\n {"value" : "14", "targetDate": "2020-01-02"}\n{"value" : "12", "targetDate": "2020-01-03"}';
+const txtTask4Values = document.getElementById('id_txttask4values');
+const task4Id = document.getElementById('task4_result');
 
-const mapData = mapInput
+txtTask4Values.value = '{"value" : "10", "targetDate": "2020-01-01"}\n {"value" : "14", "targetDate": "2020-01-02"}\n{"value" : "12", "targetDate": "2020-01-03"}';
+
+const btnTask4Result = document.getElementById('btn_task4_result');
+btnTask4Result.addEventListener("click", btnTask4ReultClick)
+document.getElementById('btn_task4_clear').addEventListener("click", btnTask4ClearClick);
+
+function btnTask4ReultClick(evt){
+  task4Id.innerHTML = '';
+  if(txtTask4Values.value != null && txtTask4Values.value != ''){
+    const mapData = txtTask4Values.value
               .split('\n')
               .filter((t) => !!t)
               .map((i) => JSON.parse(i));
 
-let mapArray = new Map();
-mapData.forEach(function(item){
-  mapArray.set(item.targetDate, item);
-});
+    let mapArray = new Map();
+    mapData.forEach(function(item){
+      mapArray.set(item.targetDate, item);
+    });
 
-console.log('---------------- Task 4 Result --------------');
-const objTemp = Object.fromEntries(mapArray);
-console.log(JSON.stringify(objTemp));
-const task4Id = document.getElementById('task4_result');
-task4Id.innerHTML = JSON.stringify(objTemp);
+    console.log('---------------- Task 4 Result --------------');
+    const objTemp = Object.fromEntries(mapArray);
+    console.log(JSON.stringify(objTemp));
+    task4Id.innerHTML = JSON.stringify(objTemp);
+  }
+};
 
+function btnTask4ClearClick(evt){
+  txtTask4Values.value = '';
+  task4Id.innerHTML = '';
+};
+
+btnTask4Result.dispatchEvent(new Event('click'));
 
 // ------------- Task 5 -------------
 
 console.log('---------------- Task 5 Result --------------');
-const element = document.getElementById('txt-area');
 
-element.addEventListener('input', callFun);
+const txtTask5Values = document.getElementById('id_txttask5values');
+const task5Id = document.getElementById('task5_result');
 
-const elementBtn = document.getElementById('btnCheck');
-elementBtn.addEventListener('click', callBtn);
-//let numInput = [5,4,3,2,1];
-let numInput = []
-function callFun(event) {  /* javascript */
-  //let numInput = [5,4,3,2,1];
-  numInput = event.currentTarget.value
-              .split('\n')
+txtTask5Values.value = '5,4,3,2,1';
+
+const btnTask5Result = document.getElementById('btn_task5_result');
+btnTask5Result.addEventListener("click", btnTask5ReultClick)
+document.getElementById('btn_task5_clear').addEventListener("click", btnTask5ClearClick);
+
+function btnTask5ReultClick(evt){
+  task5Id.innerHTML = '';
+  if(txtTask5Values.value != null && txtTask5Values.value != ''){
+    numInput = txtTask5Values.value
+              .split(',')
               .filter((t) => !!t)
               .map((i) => JSON.parse(i));
-}
-
-function callBtn(){
-  const task5Id = document.getElementById('task5_result');
-  task5Id.innerHTML = '';  
-  numInput = [...numInput].sort((a, b) => a - b);
-  console.log('sorted array - ' + numInput);
-  for(let i = 1 ; i <= numInput.length ; i++){
-    const slicedArray = numInput.slice(0, i);
-    let output = getMedian(slicedArray)
-    console.log(output);
-    task5Id.innerHTML += '<br/>';  
-    task5Id.append(output);
+              
+    numInput = [...numInput].sort((a, b) => a - b);
+    console.log('sorted array - ' + numInput);
+    for(let i = 1 ; i <= numInput.length ; i++){
+      const slicedArray = numInput.slice(0, i);
+      let output = getMedian(slicedArray)
+      console.log(output);
+      task5Id.innerHTML += '<br/>';  
+      task5Id.append(output);
+    }
   }
-}
+};
 
+function btnTask5ClearClick(evt){
+  txtTask5Values.value = '';
+  task5Id.innerHTML = '';
+};
+
+let numInput = []
 function getMedian(arr) {
   const mid = Math.floor(arr.length / 2),
     if(arr.length % 2 !== 0){
@@ -128,4 +187,5 @@ function getMedian(arr) {
       return (arr[mid - 1] + arr[mid]) / 2;
     }
 };
+btnTask5Result.dispatchEvent(new Event('click'));
 
